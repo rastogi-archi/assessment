a55f12b790d4a5f3274a6c84b8bd9245ed4121a4
Archi Rastogi <archirastogi@Archis-MacBook-Air.local>
2026-05-05 18:50:02 +0530

    added url of frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ import { isAuth } from './Middlewares/Auth.middlewares.js';
 const app = express();
 
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : ['http://localhost:5173',
+      'https://assessment-theta-coral.vercel.app/login',
+      
+    ],
     methods : ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders : ['Content-Type', 'Authorization']
 }))
